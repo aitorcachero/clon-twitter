22e36693804930aa43c936eb2465ef569fe7e046
Aitor Cachero <aitor.cachero@gmail.com>
2024-03-21 22:41:30 +0100

    change on index

--- .history/backend/src/app_20240320131734.js
+++ .history/backend/src/app_20240320131734.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Bienvenido al clon de Twitter moderno!'));
+app.get('/', (req, res) => res.send('Express on Vercel'));
 
 app.use('/users', usersRoutes);
 app.use('/tweets', tweetsRoutes);
