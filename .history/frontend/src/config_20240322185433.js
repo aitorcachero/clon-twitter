40d9239e1b8ca351244389d7715568b5350d64ec
Aitor Cachero <aitor.cachero@gmail.com>
2024-03-22 18:54:40 +0100

    revert APIurl

---
+++ .history/frontend/src/config_20240322185433.js
@@ -0,0 +1,7 @@
+const APIUrl = 'https://clon-twitter-production.up.railway.app';
+const userLocalStorageKey = 'twitterClone';
+
+export { APIUrl, userLocalStorageKey };
+
+//const APILocal = 'http://localhost:3000';
+//const APIUrl = 'https://clon-twitter-production.up.railway.app';
