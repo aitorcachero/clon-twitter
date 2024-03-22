dde042e088e8e6e604afd53dde1cb3fadbacfd82
Aitor Cachero <aitor.cachero@gmail.com>
2024-03-22 23:31:47 +0100

    refactor tops

--- frontend/src/components/TopsTweetsComponen/TopsTweetsComponen.jsx
+++ frontend/src/components/TopsTweetsComponen/TopsTweetsComponen.jsx
@@ -18,23 +18,26 @@ export default function TopsTweetsComponent({ list }) {
   };
 
   return (
-    <div className="w-[350px] md:w-[500px] border-2 border-blue-900 rounded-lg p-10 shadow-black shadow-xl bg-blue-950">
-      <div className="w-full rounded-lg bg-black p-4 mb-4">
-        <h2 className="text-center">TOP TWEETERS</h2>
-      </div>
-      {list &&
-        list.map((item, i) => (
-          <div
-            key={item.id}
-            className="flex justify-between items-center border-b-[1px] p-2"
-          >
-            <img src={getIcon(i)} className="w-10"></img>
-            <NavLink to={`/user/${item.username}`}>
-              <span>@{item.username.toLowerCase()}</span>
-            </NavLink>
-            <span className="font-semibold text-xl">{item.tweets}</span>
+    <>
+      {list && list.length > 0 && (
+        <div className="w-[350px] md:w-[500px] border-2 border-blue-900 rounded-lg p-10 shadow-black shadow-xl bg-blue-950">
+          <div className="w-full rounded-lg bg-black p-4 mb-4">
+            <h2 className="text-center">TOP SEGUIDORES</h2>
           </div>
-        ))}
-    </div>
+          {list.map((item, i) => (
+            <div
+              key={item.id}
+              className="flex justify-between items-center border-b-[1px] p-2"
+            >
+              <img src={getIcon(i)} className="w-10" alt="Icon"></img>
+              <NavLink to={`/user/${item.username}`}>
+                <span>@{item.username.toLowerCase()}</span>
+              </NavLink>
+              <span className="font-semibold text-xl">{item.tweets}</span>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
