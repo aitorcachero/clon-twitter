8384705b3adb549bc33b9531986412e9c92abf0d
Aitor Cachero <aitor.cachero@gmail.com>
2024-03-22 17:08:26 +0100

    change post button

--- .history/frontend/src/components/ButtonComponent/ButtonComponent_20240322170724.jsx
+++ .history/frontend/src/components/ButtonComponent/ButtonComponent_20240322170724.jsx
@@ -5,7 +5,7 @@ export default function ButtonComponent({ text, click }) {
     <button
       id="add-to-calendar"
       aria-label="agregar al calendario se abrirá ventana flotante"
-      className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-green-700 transition duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+      className=""
       onClick={click}
     >
       {' '}
