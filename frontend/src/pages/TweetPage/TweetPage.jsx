f1bfe034b2d73433136632220e9c9112d86b45d4
Aitor Cachero <aitor.cachero@gmail.com>
2024-03-22 19:08:49 +0100

    delete history

--- frontend/src/pages/TweetPage/TweetPage.jsx
+++ frontend/src/pages/TweetPage/TweetPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import CardTwitter from '../../components/CardTwitter/CardTwitter';
 import useTweet from '../../hooks/useTweets';
